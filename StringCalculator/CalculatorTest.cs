f461235e7fd901004bac253fb7d194855bef1f73
MayerlySalas0615 <mayerly.salas@sinco.com.co>
2025-10-22 15:41:47 -0500

    🟥 3ra Prueba Numero Negativos

--- StringCalculator/CalculatorTest.cs
+++ StringCalculator/CalculatorTest.cs
@@ -5,6 +5,7 @@ public class CalculatorTest
     [Theory]
     [InlineData("1")]
     [InlineData("2")]
+    [InlineData("456")]
     public void Cadena_String_1_Devuelve_1(string cadenaTexto)
     {
         //Act
@@ -13,7 +14,25 @@ public class CalculatorTest
         //Assert
         Assert.Equal(cadenaTexto, resultado);
     }
-    
+
+    [Fact]
+    public void Cadena_String_menos2_devuelve_menos2()
+    {
+        //Arrange
+        string cadena = "-2";
+        
+        //Act
+        string resultado = CadenaNegativos(cadena);
+        
+        //Assert
+        Assert.Equal(cadena, resultado);
+    }
+
+    private string CadenaNegativos(string cadena)
+    {
+        throw new NotImplementedException();
+    }
+
     private string CadenaStringDevolver(string input)
     {
         return input;
--- StringCalculator/obj/Debug/net9.0/StringCalculator.AssemblyInfo.cs
+++ StringCalculator/obj/Debug/net9.0/StringCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4be924ed08ede42124dfcf9fa4f26cefcf8ca94a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14b5ca21f3db4b54e5426bf0db9c50adc9d6af04")]
 [assembly: System.Reflection.AssemblyProductAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
